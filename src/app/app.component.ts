d24369de19ac70c8366fd9cd6d2bd323b173dd44
FelipeVillalobosF <felipevillalobos.figueroa@gmail.com>
2025-11-16 15:11:07 -0300

    se remueve RourteOutLet de app-component y se inclute BostsTrap en el index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { InvoiceComponent } from './components/invoice/invoice.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [InvoiceComponent ,CommonModule, RouterOutlet],
+  imports: [InvoiceComponent ,CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
